76d7e3d74176024fac8dd109a87c4d34ae907fb9
Emily Lao <emilylao@ucsb.edu>
2025-12-26 16:33:32 -0800

    setting up next.js and tailwind css

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="text-3xl font-bold text-purple-600">
+        Tailwind v4 is working
+      </div>
+    </main>
+  );
+}
